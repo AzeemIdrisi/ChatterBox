42738774c4fba0816a0edf62422a3587a25243c9
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-12-02 21:25:43 +0530

    Deployment changes

--- IPEC_ChatterBox/settings.py
+++ IPEC_ChatterBox/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)zbtj=jnqer$l$te=*f_-v$**v&h)0jf!i(x%%_tzm%gk6-+b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "ipecchat.pythonanywhere.com"]
 
 
 # Application definition
@@ -117,8 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "static"
+
+STATIC_ROOT = BASE_DIR / "staticfilesoutput"
