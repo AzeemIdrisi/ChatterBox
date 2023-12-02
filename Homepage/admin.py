50aecfe53e56a869356de3b78dbde08324980668
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-12-02 19:17:18 +0530

    Added Reply Model and Loaded into Template

--- Homepage/admin.py
+++ Homepage/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Message
+from .models import User, Message, Reply
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Message)
+admin.site.register(Reply)
