5b0b8c3d03059a260271d57b095bec1e2d67cf59
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-12-07 19:19:12 +0530

    Color change for reply

--- IPEC_ChatterBox/settings.py
+++ IPEC_ChatterBox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)zbtj=jnqer$l$te=*f_-v$**v&h)0jf!i(x%%_tzm%gk6-+b_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "ipecchat.pythonanywhere.com"]
 
