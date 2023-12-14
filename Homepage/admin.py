87d18c4b55392104789bd9a54c4506d547a24a41
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-12-14 14:29:44 +0530

    Message count

--- Homepage/admin.py
+++ Homepage/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import User, Message, Reply
 
 # Register your models here.
-admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "uid",
+        "message_count",
+        "last_message_time",
+    ]
+
+    def message_count(self, obj):
+        return obj.message_set.count()
+
+    def last_message_time(self, obj):
+        last_message = obj.message_set.last()
+        return last_message.time if last_message else None
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Message)
 admin.site.register(Reply)
