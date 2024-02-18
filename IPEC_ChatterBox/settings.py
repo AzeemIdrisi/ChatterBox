12d670fde645edbc129694eb1d4607eea34b3fe7
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-02-18 13:42:28 +0530

    Collect Static Directory Change

--- IPEC_ChatterBox/settings.py
+++ IPEC_ChatterBox/settings.py
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -130,6 +128,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
 
-STATIC_ROOT = BASE_DIR / "staticfilesoutput"
+STATIC_ROOT = BASE_DIR / "static"
